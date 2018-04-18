0cf9fd023be127a06c331ec8029604af4c1f0b76
mackfire_me <mackfire_me@hotmail.com>
2018-04-18 22:40:58 +0800

    wo

--- Lexer/LexicalAnalysis.cpp
+++ Lexer/LexicalAnalysis.cpp
@@ -5,6 +5,12 @@
 // https://zhuanlan.zhihu.com/p/19879002
 
 LexicalAnalysis::LexicalAnalysis(const char* start, const char* end)
+	: m_Start(start)
+	, m_End(end)
+	, m_State(LexState::NORMAL)
+	, m_Line(0)
+	, m_PosInLine(0)
+	, m_Position(0)
 {
 }
 
@@ -17,10 +23,20 @@ std::unique_ptr<Token> LexicalAnalysis::Next() {
 
 }
 
-bool LexicalAnalysis::Read(char ch) throw(LexicalException)
+void LexicalAnalysis::Read(char ch) throw(LexicalException)
 {
-	bool next = true;
+	bool movenext = true;
 	Token::TokenType type = Token::TokenType::NONE;
+
+	if (m_Start + m_Position >= m_End) {
+		type = Token::TokenType::ENDS;
+		movenext = false;
+		return;
+	}
+
+	if (!isprint(ch) || '\n' == ch) {
+		return;
+	}
 
 	switch (m_State) {
 	case LexState::NORMAL:
@@ -32,18 +48,23 @@ bool LexicalAnalysis::Read(char ch) throw(LexicalException)
 		}
 		else if ('\'' == ch || '\"' == ch) {
 			m_State = LexState::STRING;
-			next = false;
+			movenext = false;
 		}
 		else if (';' == ch) {
 			m_State = LexState::ANNOTATION;
 		}
 		else if ('`' == ch) {
 			m_State = LexState::REGEX;
+			movenext = false;
 		}
 		else if ('\n' == ch) {
 			type = Token::TokenType::NEWL;
+			if (NextChar() == '\r') m_Position++;
 			m_Line++;
 			m_PosInLine = 0;
+		}
+		else if ('\r' == ch) {
+			m_State = LexState::NORMAL;
 		}
 		else if (0 == ch) {
 			type = Token::TokenType::ENDS;
@@ -61,25 +82,68 @@ bool LexicalAnalysis::Read(char ch) throw(LexicalException)
 			throw LexicalException(ch, m_Line, m_PosInLine);
 		}
 		break;
-	case LexState::OBJECT :
+	case LexState::OBJECT:
 		break;
-	case LexState::KEYWORD :
+	case LexState::KEYWORD:
 		break;
-	case LexState::IDENTIFIER :
+	case LexState::IDENTIFIER:
+		if (isalnum(ch) || '_' == ch) {
+			m_Buffer << ch;
+		}
+		else {
+			movenext = false;
+			m_State = LexState::NORMAL;
+		}
 		break;
-	case LexState::SIGN :
+	case LexState::SIGN:
 		break;
-	case LexState::STRING :
+	case LexState::STRING:
+		if ('\r' == ch || '\n' == ch || '}' == ch) {
+			throw LexicalException(ch, m_Line, m_PosInLine);
+		}
+		else {
+			m_Buffer << ch;
+			if (m_Buffer[1] == ch) {
+				type = Token::TokenType::STRI;
+				m_State = LexState::NORMAL;
+			}
+		}
 		break;
-	case LexState::NUMBER :
+	case LexState::NUMBER:
 		break;
-	case LexState::MATRIX :
+	case LexState::MATRIX:
 		break;
-	case LexState::REGEX :
+	case LexState::REGEX:
+		if ('\r' == ch || '\n' == ch || '}' == ch) {
+			throw LexicalException(ch, m_Line, m_PosInLine);
+		}
+		else {
+			m_Buffer << ch;
+			if (m_Buffer[1] == ch) {
+				type = Token::TokenType::STRI;
+				m_State = LexState::NORMAL;
+			}
+		}
 		break;
-	case LexState::SPACE :
+	case LexState::SPACE:
+		if (isspace(ch)) {
+			m_Buffer << ch;
+		}
+		else {
+			type = Token::TokenType::SPAC;
+			m_State = LexState::NORMAL;
+			movenext = false;
+		}
 		break;
-	case LexState::ANNOTATION :
+	case LexState::ANNOTATION:
+		if ('\r' == ch || '\n' == ch) {
+			type = Token::TokenType::ANNO;
+			m_State = LexState::NORMAL;
+			movenext = false;
+		}
+		else {
+			m_Buffer << ch;
+		}
 		break;
 	}
 
@@ -87,9 +151,7 @@ bool LexicalAnalysis::Read(char ch) throw(LexicalException)
 		AddToken(type);
 	}
 
-	if (next) {
+	if (movenext) {
 		m_Position++;
 	}
-
-	return next;
 }
--- Lexer/LexicalAnalysis.h
+++ Lexer/LexicalAnalysis.h
@@ -5,7 +5,7 @@ class LexicalException : public std::exception {
 public:
 	LexicalException(char ch, int line, int pos) {
 		std::stringstream stm;
-		stm << "Lexical error in line " << line << " char "<< pos<< ": \'" << ch << "\'";
+		stm << "Lexical error in line " << line << " char " << pos << ": \'" << ch << "\'";
 		exception(stm.str().c_str());
 	}
 };
@@ -26,18 +26,36 @@ class LexicalAnalysis
 		ANNOTATION,
 	};
 
+	const char* m_Start;
+	const char* m_End;
 	int m_Line;
 	int m_PosInLine;
 	int m_Position;
 	LexState m_State;
 	std::vector<TokenPtr> m_Tokens;
 	std::stringstream m_Buffer;
+
 	void AddToken(Token::TokenType type) {
-		TokenPtr token = std::make_unique<Token>(type, m_Buffer.str());
-		m_Tokens.push_back(token);
+		AddToken(type, m_Buffer.str());
 		m_Buffer.str("");
 	}
-	bool Read(char ch) throw(LexicalException);
+	void AddToken(Token::TokenType type, const std::string& value) {
+		TokenPtr token = std::make_unique<Token>(type, value);
+		m_Tokens.push_back(token);
+	}
+	void Read(char ch) throw(LexicalException);
+	char NextChar() const {
+		if (m_Start + m_Position + 1 < m_End)
+			return m_Start[m_Position + 1];
+		else
+			throw "Position can't move after the buffer.";
+	}
+	char PrevChar() const {
+		if (m_Position > 0)
+			return m_Start[m_Position - 1];
+		else
+			throw "Position can't move ahead the buffer.";
+	}
 
 public:
 	LexicalAnalysis(const char* start, const char* end);
